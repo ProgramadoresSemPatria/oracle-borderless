38f8cd0c0dc85320901ed959c89a04c54427dd30
Duanne Moraes <duanne.souza@gmail.com>
2026-07-20 13:32:05 -0400

    feat(frontend): add About & Sources page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./features/landing/LandingPage";
+import AboutPage from "./features/about/AboutPage";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/about" element={<div>About</div>} />
+      <Route path="/about" element={<AboutPage />} />
       <Route path="/knowledge" element={<div>Knowledge</div>} />
       <Route path="/oracle" element={<div>Oracle</div>} />
       <Route path="/oracle/:conversationId" element={<div>Oracle</div>} />
